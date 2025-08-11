21533789f3bd197ddca8ad3e3935ba2edfc2c6ef
Glutix <ferreyra.gluti@gmail.com>
2025-08-11 12:37:57 -0300

    login funcional, y modularizacion de archivo estaticos

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from .forms import CreateUserForm, CustomLoginForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -22,3 +23,17 @@ def register(request):
         form = CreateUserForm()
 
     return render(request, "register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = CustomLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            print(user)
+            login(request, user)
+            return redirect("/")
+    else:
+        form = CustomLoginForm()
+
+    return render(request, "login.html", {"form": form})
